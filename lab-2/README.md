4ac063619ebc16cb706d20b9ce992624b8e1d832
Dimi Racordon <dimitri.racordon@gmail.com>
2025-09-24 21:30:30 +0200

    Add lab 2

---
+++ lab-2/README.md
@@ -0,0 +1,215 @@
+---
+title: "Lab 2 -- ARM Assembly 2"
+author: [Dr Dimi Racordon]
+date: "22.9.2025"
+version: "1.0.0"
+
+module: "202"
+ue: "202.1"
+course: "Operating Systems"
+
+# LaTex specific
+fontsize: 10pt
+caption-justification: centering
+---
+
+<style>
+r { color: Red }
+y { color: Yellow }
+FIXME { color: Yellow }
+TODO {color: Blue}
+</style>
+
+# Objective
+
+The objective of this laboratory is to familiarize yourself with the basics of low-level memory management using the ARM assembly language.
+Specifically, we'll learn how to:
+
+- manipulate the stack pointer;
+- allocate and deallocate heap memory; and
+- use virtual files.
+
+The estimated duration of this lab is **4 periods**.
+The result of Task 5 must be submitted on [isc.hevs.ch/learn](https://isc.hevs.ch/learn) no later than Sunday 12th October at 23:59 (CEST).
+
+# Setup
+
+Assembly languages typically target a family of related CPUs.
+As we will use 64-bit ARM assembly, you will have to setup a specific architecture (i.e., [AArch64](https://developer.arm.com/documentation/102374/0102)) to complete this lab.
+Fortunately, we can use containers to emulate such an architecture on top of most modern systems.
+
+1. Install [Docker Desktop](https://www.docker.com/) and [Visual Studio Code (VS Code)](https://code.visualstudio.com) on your machine.
+2. In VS Code, install the [Dev Containers](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers) extension.
+3. Download (or clone using git) the following repository: [https://github.com/ISC-HEI/202.1-os](https://github.com/ISC-HEI/202.1-os).
+4. Open the `lab-2` folder of this repository with VS Code and click on "Reopen in Container" in the bottom-right dialogue. If the dialog does not show, you can press `Ctrl+Shift+P` to open the command palette and type "Reopen in Container".
+
+> Make sure to open the lab folder **directly** to let VS Code detect that a Docker container has been configured.
+
+VS Code will reopen and start a Docker container configured to run Linux on a 64-bit ARM architecture (emulated if necessary).
+Once the container is ready, open a terminal in VS Code and run the following command to confirm that your system is up and running:
+
+```bash
+uname -m
+```
+
+The command should print `aarch64` on your terminal.
+
+> It should be possible to complete this lab if you are running Linux on a 64-bit ARM architecture natively.
+> Using a container is nonetheless recommended to ensure that your environment is properly configured with all the tools that will be necessary for this lab and its successors.
+
+# Manipulating the Call Stack
+
+In Linux (and in most other systems), each process is given access to a fixed pool of memory that is managed like a LIFO (last-in-first-out) stack.
+This pool of memory is often called the "call stack" or simply the "stack".
+Its main is to store the value of local variables that either do not fit into registers or must persist after a function call.
+The command-line arguments of the process are also stored on the stack.
+
+## Allocating Memory
+
+The stack grows downward: the bottom of the stack has a higher address than its top.
+The special register `sp` stores a pointer to the top of the stack.
+Allocating memory on the stack amounts to decrementing the value in `sp` whereas deallocating memory amounts to incrementing that value.
+The architecture's specification requires that `sp` be aligned at a 16-byte boundary.
+In other words, **`sp` must always be a multiple of 16**.
+
+The following program uses the stack to swap the contents of two registers:
+
+```asm
+.text
+.global _start
+_start:
+  sub  sp, sp, #16            // push 16 bytes to the stack
+  mov  x0, #93
+  mov  x8, #42
+  stp  x0, x8, [sp]           // store x0 and x8 on the stack
+  ldp  x8, x0, [sp]           // load the contents of the stack to x8 and x0
+  add  sp, sp, #16            // pop 16 bytes from the stack
+  svc  #0
+```
+
+## Command-line Arguments
+
+The operating pushes the command-line arguments of a process on the stack at the beginning of its execution.
+The top of the stack (i.e, the value pointed to by `sp`) contains the number of command-line arguments.
+Pointers to these arguments are stored next.
+
+For example, if one executes `ls -a somewhere`, the stack will be laid out so that:
+
+- `sp` points to 3;
+- `sp+0x08` points to a null-terminated string `/bin/ls`;
+- `sp+0x10` points to a null-terminated string `-a`; and
+- `sp+0x18` points to a null-terminated string `somewhere`.
+
+> Readers familiar with C/C++ may note that this particular layout is reminiscent of the traditional parameter list declared on the `main` function of a program: `int main(int argc, char** argv)`.
+> That is no coincidence! `argc` will denote the number of arguments and `argv` will be an array of pointers to the values of these arguments.
+
+The following excerpt shows how to get a pointer to the value of first command line argument, which always denote the name of the executable:
+
+```asm
+_start:
+  add  x0, sp, #8
+  ldr  x1, [x0]               // x0 = argv
+  ldr  x2, [x1]               // x1 = argv[0]
+```
+
+## Calling Functions
+
+The [Procedure Call Standard](https://developer.arm.com/documentation/102374/0103/Procedure-Call-Standard) specifies the registers whose values should not be corrupt (i.e., overridden) when a function is called.
+Specifically, registers `x19` to `x28` are said to be *callee-saved*, meaning that any function using these registers should restore their original value before returning.
+Further, registers `x29` and `x30` denote have special significance:
+
+- `x29` denotes the *frame pointer*, which identifies the start of the current call frame; and
+- `x30` denotes the *link register*, which identifies where control-flow should return after a function call.
+
+> Forgetting to propertly restore the link register may prevent the `ret` instruction from behaving properly!
+
+It is customary at the beginning of a function to store `x29` and `x30` on the stack and restore these registers before the function returns.
+The following excerpt illustrates:
+
+```asm
+_factorial:
+  stp  x29, x30, [sp, #-32]!  // store fp and lr on the stack
+  mov  x29, sp                // assign the frame pointer
+  str  x28, [sp, #16]         // store x28 on the stack
+  mov  x28, x0                // write x0 to a callee-save register
+  subs x0, x0, #1             // x0 -= 1
+  ble  _factorial.one         // if x0 <= 1 { goto _factorial.one }
+  bl   _factorial             // x0 := factorial(x0 - 1)
+  mul  x0, x0, x28            // x0 *= x28
+  b    _factorial.ret         // goto _factorial.ret
+_factorial.one:
+  mov x0, #1                  // x0 = 1
+_factorial.ret:
+  ldr  x28, [sp, #16]         // restore x28
+  ldp  x29, x30, [sp], #32    // restore fp and lr
+  ret                         // return
+```
+
+## Addressing Modes
+
+There are several ways to specify the address from/ro which an instruction should read/write.
+Some of them are shown in the table below, where `R` stands for either an **extended** general-purpose register (e.g., `x5`) or the stack pointer (i.e., `sp`).
+More documentation can be found on [this page](https://developer.arm.com/documentation/102374/0103/Loads-and-stores---addressing).
+
+| Name | Syntax | Effect |
+| - | - | - |
+| Register | `[R]` | accesses `R` |
+| Register with offset | `[R, #i]` | accesses `R + i` |
+| Register with pre-increment | `[R, #i]!`  | computes `R += i` and accesses `R` |
+| Register with post-increment | `[R], #i`  | accesses `R` and computes `R += i` |
+
+## Task 1 -- Fold
+
+`foldright` is an algorithm that is used to "fold" the elements of a collection into a single value, using some combinator.
+In Scala, for example, the expression `ArraySeq(1, 2, 3).foldRight(0)(_ + _)` computes `6`.
+In this example, the collection is an array of 3 elements and the combinator is the integer addition.
+The algorithm is called `foldright` because elements are visited right to left.
+Written imperatively, the above sequence is equivalent to the following block:
+
+```scala
+var accumulator = 0
+for x <- ArraySeq(1, 2, 3) do
+  accumulator += x
+accumulator
+```
+
+Open the file `fold.s` and locate the label `_foldright`, which marks the star of a function.
+Your task is to implement the definition of this function that satisfies its specification.
+You are free to opt for an iterative or recursive approach.
+
+To test your function, you can compile the program and give it a list of numbers.
+A successful implementation will write their sum on the console.
+A usage example is shown below.
+
+```bash
+./compile.sh fold.s
+./fold sum 1 2 3
+```
+
+Do not hesitate to use other functions to help you develop and debug your implementation.
+In particular, you can use `_printa` to print an address and `_printn` to print a number.
+
+## Task 2 -- Insert
+
+Notice that `_foldright` is a higher order function.
+Its fifth parameter, which represent the combinator, is expected to the the address of a function accepting its parameters in registers `x0` and `x1`.
+
+- If you type `./fold sum ...`, the program uses `_add` as the combinator, which simply adds the value of each element to the accumulator.
+- If you type `./fold sort ...`, the program uses `_insert` as the combinator, which inserts elements into a binary search tree.
+
+Internall, `_insert` calls `_tree_insert` to process each element.
+Your task is to implement the latter.
+
+In the same file as the one in which you have implemented `_foldright`, locate the label `_tree_insert` and write an implementation that satisfies the functions's specification.
+
+To test your function, you can compile the program and give it a list of numbers.
+A successful implementation will write them in increasing order.
+A usage example is shown below.
+
+```bash
+./compile.sh fold.s
+./fold sort 1 5 3 7 2
+```
+
+Once you're done, submit your while program, which should the implementations of `_foldright` and `_tree_insert`, on [isc.hevs.ch/learn](https://isc.hevs.ch/learn).
+Your lab will be graded on the basis of the correctness and clarity of your implementation.
---
+++ lab-2/fold.s
@@ -0,0 +1,502 @@
+.data
+  mm:
+    .quad 0 // start of heap
+    .quad 0 // current break
+  usage:
+    .ascii "usage: fold [sum|sort] n...\n\0"
+  sum_text:
+    .ascii "sum\0"
+  sort_text:
+    .ascii "sort\0"
+
+.text
+.global _start
+_start:
+  // Read command line arguments and exit unless there are at least two, excluding the name of the
+  // executable. The first argument will be read as the command and the remainder will be parsed
+  // as input numbers.
+  ldr   x0, [sp]
+  mov   x19, sp
+  sub   x20, x0, #2
+  add   x21, sp, #24
+  cmp   x20, #0
+  ble   _usage
+
+  // Initialize the heap.
+  bl    _init_heap
+
+  // Allocate enough memory to store each input argument on the stack. We're allocating an even
+  // number of elements to make sure we satisfy the alignment requirement of the stack pointer.
+  lsl   x0, x20, #3
+  mov   x1, #4
+  bl    _round_up_to_next_multiple_of_2n
+  sub   sp, sp, x0
+
+  // Parse input numbers to numbers and store write them to the stack.
+  mov   x22, sp
+  mov   x23, x20
+_l0:
+  cbz   x23, _l1
+  sub   x23, x23, #1
+  ldr   x0, [x21], #8
+  bl    _atoi
+  str   x0, [x22], #8
+  b     _l0
+_l1:
+
+  // Determine which command should be executed.
+  ldr   x0, =sum_text
+  ldr   x1, [x19, #16]
+  bl    _strcmp
+  cbnz  x0, _l2
+  ldr   x0, =sort_text
+  ldr   x1, [x19, #16]
+  cbnz  x0, _l3
+  b     _usage
+_l2:
+  mov   x5, #1000
+  b     _l4
+_l3:
+  mov   x5, #1001
+_l4:
+
+  // Allocate memory for the accumulator. For the "sum" command, this memory will hold the sum of
+  // each input number. For the "sort" command, it will hold a pointer to a binary search tree.
+  mov   x0, #0
+  stp   x0, x0, [sp, #-16]!
+
+  // Configure the arguments that are identical for both commands.
+  add   x0, sp, #16
+  mov   x1, #8
+  mov   x2, x20
+  mov   x3, sp
+
+  cmp  x5, #1001
+  beq   _sort
+
+  // _foldright(0, (a, b) => *a += *b)
+  adrp	x5, _add
+	add	  x4, x5, :lo12:_add
+  bl    _foldright
+
+  // Load and print the result.
+  ldr   x0, [x0]
+  bl    _printn
+  b     _success
+
+_sort:
+  // _foldright(0, (a, b) => *a.insert(*b))
+  adrp  x5, _insert
+  add   x4, x5, :lo12:_insert
+  bl    _foldright
+
+  // Load and print the result.
+  ldr   x0, [x0]
+  bl    _printt
+  b     _success
+
+// Exit with a status 0.
+_success:
+  mov   w0, #0
+  mov   w8, #93
+  svc   #0
+
+// Show the executable's usage and exit with a status 1.
+_usage:
+  ldr   x0, =usage
+  bl    _strlen
+  mov   x2, x0
+  ldr   x1, =usage
+  mov   x0, #1
+  mov   w8, #64
+  svc   #0
+
+// Exit with a failure status 1.
+_failure:
+  mov   w0, #1
+  mov   w8, #93
+  svc   #0
+
+/// Combines the the elements a buffer into an accumulator by applying the given function on the
+/// accumulator and each element, from right to left.
+///
+/// This function "folds" a sequence of elements from right to left into an accumulator using an
+/// updating function provided by its caller. The updating function accepts the address of the
+/// accumulator in `x0` and the address of an element in `x1`, combines the value at `x1` with the
+/// value at `x0`, and writes the result in `x0`. The addresses of the `n`-th (with `n` less than
+/// the number of elements in the buffer) is computed by offsetting the buffer's base address by
+/// `n * s`, where `s` is the size of an element.
+///
+/// When the function returns, `x0` contains the address of the accumulator that was initially
+/// passed in `x3`. The result of other argument registers is unspecified.
+///
+/// The following is a possible recursive implementation of `_foldright` in C:
+///
+///     void* foldright(void* xs, int s, int n, void* a, void(*f)(void*, void*)) {
+///       if (n != 0) {
+///         char* p = (char*)xs + s * --n;
+///         f(a, p);
+///         foldright(xs, s, n, a, f);
+///       }
+///       return a;
+///     }
+///
+/// - Parameters:
+///   - x0 The address of a buffer.
+///   - x1 The size of each element in the buffer pointed to by `x0`, , which is less than 2^16
+///   - x2 The number of elements in the buffer pointed to by `x0`, which is less than 2^48.
+///   - x3 The address of the initial accumulator value.
+///   - x4 The address of a function that combines an element with the accumulator.
+///
+/// - Complexity: O(n) where n is the number of elements in the buffer.
+_foldright:
+  // TODO
+  ret
+
+/// Inserts the value of `x1` in the binary search tree rooted at `x0`.
+///
+/// If `x0` is null, a new node is allocated and its address is returned in `x0`. Otherwise, the
+/// value in `x1` is inserted in either the left or right child of the node at `x0` and the value
+/// of `x0` is preserved.
+///
+/// A node is a triple `(lhs, rhs, value)` where `lhs` and `rhs` are (possibly null) pointers to
+/// the node's children and `value` is the value assigned to that node.
+///
+/// - Complexity: O(log n) where n is the number of elements in the tree.
+_tree_insert:
+  // TODO
+  ret
+
+/// Computes the sum of the integers stored at the addresses contained in `x0` and `x1` and writes
+/// the result at the address contained in `x0`.
+_add:
+  ldr   x2, [x0]
+  ldr   x3, [x1]
+  add   x4, x2, x3
+  str   x4, [x0]
+  ret
+
+/// Inserts the number at the address in `x1` into the binary search tree whose address is stored
+/// at the addess in `x2`.
+_insert:
+  stp   x29, x30, [sp, #-32]!
+  mov   x29, sp
+  str   x0, [sp, #16]
+  ldr   x0, [x0]
+  ldr   x1, [x1]
+  bl    _tree_insert
+  ldr   x1, [sp, #16]
+  str   x0, [x1]
+  ldp   x29, x30, [sp], #32
+  ret
+
+/// Computes the smallest multiple of 2^n greater than or equal to `x0`, where `n` is the value of
+/// `x1`, and writes the results to `x0`.
+///
+/// For example:
+/// - Given `x0 = 10` and `x1 = 4`, the result is 16.
+/// - Given `x0 = 16` and `x1 = 4`, the result is 16.
+_round_up_to_next_multiple_of_2n:
+  mov   x2, #1
+  lsl   x3, x2, x1
+  sub   x4, x3, #1
+  and   x5, x0, x4
+  cbz   x5, _round_up_to_next_multiple_of_2n.ret
+  add   x2, x0, x4
+  lsr   x3, x2, x1
+  lsl   x0, x3, x1
+_round_up_to_next_multiple_of_2n.ret:
+  ret
+
+/// Converts the null-terminated string pointed to by `x0`, which encodes a non-negative integer in
+/// base 10, and writes the result to `x0`.
+///
+/// - Parameters:
+///   - x0 the address of a null-terminated string.
+_atoi:
+  mov   x1, #0
+	mov   x2, #0
+	mov   x3, #10
+_atoi.head:
+  ldrb	w5, [x0, x1]
+	add   x1, x1, #1
+	cbz   w5, _atoi.ret
+	sub   w5, w5, #48
+	mul   x2, x2, x3
+  add   x2, x2, x5
+	b     _atoi.head
+_atoi.ret:
+  mov   x0, x2
+  ret
+
+/// Converts the unsigned integer value in `x0` into a null-terminated string using the base in
+/// `x2` and stores the result in the array to which `x1` points.
+///
+/// The value of `x1` is preserved. On success, `x0` is equal to `x1` and `x2` contains the number
+/// of characters in the result. On failure, `x0` contains -1 and the value of `x2` is unspecified.
+///
+/// - Parameters:
+///   - x0 the value to be converted.
+///   - x1 the address of a buffer large enough to contains the output string.
+///   - x2 the numerical base (between `2` and `36`) used to represent the converted value.
+_itoa:
+  stp   x29, x30, [sp, #-32]!
+  mov   x29, sp
+
+  // if x2 < 2 || x2 > 36 { goto _itoa.err }
+  cmp   x2, #2
+  blt   _itoa.err
+  cmp   x2, #36
+  bgt   _itoa.err
+
+  // if x0 == 0 { goto _itoa.zero }
+  cbz   x0, _itoa.zero
+
+  // Converts into digits, from least to most significant, and then reverse them.
+  mov   x3, #0
+_itoa.head:
+  cbz   x0, _itoa.ret
+  udiv  x4, x0, x2			    // x4 = x0 / x2
+	msub  x5, x4, x2, x0	    // x5 = x0 % x2
+	mov   x0, x4
+  cmp   w5, #10
+  bge   _itoa.hex
+	add   w5, w5, #48
+  b     _itoa.tail
+_itoa.hex:
+  add   w5, w5, #87
+_itoa.tail:
+	strb  w5, [x1, x3]
+  add   x3, x3, #1
+	b 		_itoa.head
+_itoa.zero:
+  mov   w5, #48
+  strb  w5, [x1]
+  mov   x3, #1
+_itoa.ret:
+  mov   w5, #0
+  strb  w5, [x1, x3]
+  mov   x0, x1
+  mov   x1, x3
+  stp   x0, x1, [sp, #16]
+  bl   _reverse
+  ldp   x1, x2, [sp, #16]
+  mov   x0, x1
+  ldp   x29, x30, [sp], #32
+  ret
+_itoa.err:
+  mov   x0, #-1
+  ldp   x29, x30, [sp], #32
+  ret
+
+/// Reverses the bytes in the array that is pointed to by `x0` and whose length is `x1`.
+_reverse:
+  mov   x2, #0
+_reverse.head:
+  cmp   x1, x2
+  ble   _reverse.ret
+  sub   x1, x1, #1
+  ldrb	w5, [x0, x1]
+  ldrb	w6, [x0, x2]
+  strb  w5, [x0, x2]
+  strb  w6, [x0, x1]
+  add   x2, x2, #1
+  b     _reverse.head
+_reverse.ret:
+  ret
+
+/// Computes the lenght of the null-terminated string at `x0` and writes the result to `x0`.
+_strlen:
+  mov   x1, x0
+  mov   x0, #0
+_strlen.head:
+  ldrb	w5, [x1, x0]
+  cbz   w5, _strlen.ret
+  add   x0, x0, #1
+  b     _strlen.head
+_strlen.ret:
+  ret
+
+/// Tests whether the null-terminated strings at `x0` and `x1` are equal and writes the result to
+/// `x0` (1 if they are equal or 0 otherwise).
+_strcmp:
+  ldrb  w5, [x0], #1
+  ldrb  w6, [x1], #1
+  cmp   w5, w6
+  bne   _strcmp.ne
+  cbz   w5, _strcmp.eq
+  b     _strcmp
+_strcmp.ne:
+  mov   x0, 0
+  ret
+_strcmp.eq:
+  mov   x0, 1
+  ret
+
+/// Prints the value of `x0`.
+_printn:
+  stp   x29, x30, [sp, #-32]!
+  mov   x29, sp
+  add   x1, sp, #16
+  mov   x2, #10
+  bl    _itoa
+  mov   w5, #10
+  strb  w5, [x0, x2]
+  add   x2, x2, #14
+  mov   x0, #1
+  mov   w8, #64
+  svc   #0
+  ldp   x29, x30, [sp], #32
+  ret
+
+/// Prints the value of `x0` as an address (e.g., "0x1234").
+_printa:
+  stp   x29, x30, [sp, #-48]!
+  mov   x29, sp
+  mov   w5, #48
+  strb  w5, [sp, #24]
+  mov   w5, #120
+  strb  w5, [sp, #25]
+  add   x1, sp, #26
+  mov   x2, #16
+  bl    _itoa
+  mov   w5, #10
+  strb  w5, [x0, x2]
+  add   x2, x2, #3
+  add   x1, sp, #24
+  mov   x0, #1
+  mov   w8, #64
+  svc   #0
+  ldp   x29, x30, [sp], #48
+  ret
+
+/// Prints the contents of the binary tree rooted at `x0`, visiting its elements in in-order.
+_printt:
+  stp   x29, x30, [sp, #-272]!
+  mov   x29, sp
+  add   x1, sp, #16
+  mov   w5, 91 // '['
+  strb  w5, [x1], #1
+  bl    _printt.inner
+_printt.ret:
+  ldrb  w0, [x1, #-1]
+  cmp   w0, #32
+  bne   _printt.svc
+  sub   x1, x1, #2
+_printt.svc:
+  mov   w5, 93 // ']'
+  strb  w5, [x1], #1
+  mov   w5, 10 // '\n'
+  strb  w5, [x1], #1
+  add   x0, sp, #16
+  sub   x2, x1, x0
+  mov   x1, x0
+  mov   x0, #1
+  mov   w8, #64
+  svc   #0
+  ldp   x29, x30, [sp], #256
+  ret
+_printt.inner:
+  // x0 the tree ; x1 the string
+  stp   x29, x30, [sp, #-32]!
+  mov   x29, sp
+  cbz   x0, _printt.inner.ret
+  str   x0, [sp, #16]
+  ldr   x0, [x0]
+  bl    _printt.inner
+  ldr   x0, [sp, #16]
+  ldr   x0, [x0, #16]
+  mov   x2, #10
+  bl    _itoa
+  add   x1, x1, x2
+  mov   w5, #44 // ','
+  strb  w5, [x1], #1
+  mov   w5, #32 // ' '
+  strb  w5, [x1], #1
+  ldr   x0, [sp, #16]
+  ldr   x0, [x0, #8]
+  bl    _printt.inner
+_printt.inner.ret:
+  ldp   x29, x30, [sp], #32
+  ret
+
+/// Initializes the heap.
+///
+/// This function reserves 2KB of heap memory and initializes `mm` with pointers to the start and
+/// end of the heap memory region.
+_init_heap:
+  ldr   x2, =mm
+
+  // Get the current program break.
+  mov   x0, #0
+  mov   w8, #214
+  svc   #0
+  str   x0, [x2]
+  mov   x1, x0
+
+  // Advance the program break by 2048 bytes.
+  add   x0, x0, #2048
+  svc   #0
+  str   x0, [x2, #8]
+
+  // Fail if we could not allocate memory.
+  cmp   x0, x1
+  beq   _failure
+
+  // Store a pointer to the next free memory address at the start of the heap.
+  add  x2, x1, #8
+  str  x2, [x1]
+  ret
+
+/// Allocates on the heap as many bytes as the value of `x0` aligned at 2^n, where `n` is the value
+/// of `x1`, and writes the address of to the allocation to `x0`.
+///
+/// This function assumes that heap memory has been initialized with `_init_heap`.
+_aalloc:
+  stp   x29, x30, [sp, #-48]!
+  mov   x29, sp
+
+  // Load the current configuration of the heap.
+  ldr   x2, =mm
+  ldr   x3, [x2]      // heap.start
+  ldr   x4, [x3]      // heap.current
+  ldr   x5, [x2, #8]  // heap.end
+
+  stp   x0, x5, [sp, #16]
+  stp   x3, x4, [sp, #32]
+
+  // Adjust the start address.
+  mov   x0, x4
+  bl    _round_up_to_next_multiple_of_2n
+
+  // Do we need more memory?
+  ldp   x1, x5, [sp, #16]
+  add   x3, x0, x1
+  cmp   x3, x5
+  ble   _aalloc.bump
+  stp   x0, x3, [sp, #16]
+
+  // Ask for more memory.
+  mov   x0, x3
+  mov   x1, #11
+  bl    _round_up_to_next_multiple_of_2n
+  mov   w8, #214
+  svc   #0
+
+  // Update the heap configuration.
+  ldr   x2, =mm
+  ldr   x5, [x2, #8]
+  cmp   x0, x5
+  beq   _aalloc.fail
+  str   x0, [x2, #8]
+  ldp   x0, x3, [sp, #16]
+
+_aalloc.bump:
+  ldr   x2, [sp, #32]
+  str   x3, [x2]
+  b     _aalloc.ret
+_aalloc.fail:
+  mov   x0, #0
+_aalloc.ret:
+  ldp   x29, x30, [sp], #48
+  ret
